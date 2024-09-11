3b40b232a88affa8408b07a8885a412b1e85312a
Emmanuel Osman Bangura <emmiebangs@gmail.com>
2024-09-11 21:45:13 +0000

    bcrypt added

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import bcrypt from 'bcrypt';
+import bcrypt from '@node-rs/bcrypt';
 
 const login = async (req, res) => {
     let userLoggedIn = false;
